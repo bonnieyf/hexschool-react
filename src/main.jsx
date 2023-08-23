f05f22178f2ea67ee45e158877a92dddb7962d7a
Bonnie Wang <kusasen@gmail.com>
2023-08-23 14:43:37 +0800

    commit first week1 homework

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import Week1 from './Week1'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+      <Week1/>
+  </React.StrictMode>,
+)
